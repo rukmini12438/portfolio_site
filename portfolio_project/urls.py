951aceae2485294d311ff0ac699cce45cc8d6051
rukmini12438 <rukminigkp332@gmail.com>
2026-07-08 12:18:50 +0530

    Add re_path for serving media files
    Updated urlpatterns to include re_path for serving media files in DEBUG mode.

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve as serve_static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve_static, {'document_root': settings.MEDIA_ROOT}),
+]
